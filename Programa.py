e4ac35e95ce41a12eb58fbcea202d35f80535ff9
jaimillos <jaime.monachil@gmail.com>
2025-03-21 17:55:19 +0100

    V1.3

--- Programa.py
+++ Programa.py
@@ -9,16 +9,16 @@ biblioteca_libros = []
 
 class Libro:
     
-    def __init__(self, titulo: str, autor: str, isbn: str, disponible: bool):
+    def __init__(self, titulo: str, autor: str, isbn: str):
         self.titulo = titulo
         self.autor = autor
-        self.isbn = isbn
-        self.disponible = disponible
+        self.isbn = isbn.strip()
+        self.disponible = True
 
     def agregar(self, titulo: str, autor: str, isbn: str):
         self.titulo = titulo
         self.autor = autor
-        self.isbn = isbn
+        self.isbn = isbn.strip()
         self.disponible = True
 
     def prestar(self):
@@ -29,15 +29,15 @@ class Libro:
 
     def mostrar(self):
         for libro in biblioteca_libros:
-            print("{} ({}) - ISBN: {} - Disponible:  {}\n".format(libro.titulo, libro.autor, libro.isbn, libro.disponible))
+            print(libro)
     
     def buscar(self, isbn: str):
         for libro in biblioteca_libros:
-            if libro.isbn == isbn:
+            if libro.isbn == isbn.strip():
                 return str(libro)
 
     def __str__(self):
-        return "Título: {}; Autor: {}; isbn:{}; Disponible:{}".format(self.titulo, self.autor, self.isbn, "Sí" if self.disponible else "No") 
+        return f"{} ({}) - ISBN: {} - Disponible: {}".format(self.titulo, self.autor, self.isbn, "Sí" if self.disponible else "No") 
     
 #Clase Libros ------------------------------------------------
 # ------------------------------------------------------------
@@ -51,23 +51,23 @@ class Biblioteca:
 
     def getLibro(self, isbn: str):
         for libro in biblioteca_libros:
-            if libro.isbn == isbn:
+            if libro.isbn == isbn.strip():
                 return libro
         return None
     
     def existsLibro(self, isbn: str):
         for libro in biblioteca_libros:
-            if libro.isbn == isbn:
+            if libro.isbn == isbn.strip():
                 return True
         return False
     
     def addLibro(self, titulo: str, autor: str, isbn:str):
         #Comprobamos que no exista
         if self.existsLibro(isbn):
-            return "Ya existía el libro con ISDN:{} en el catálogo".format(isbn)
+            return f"Ya existía el libro con ISDN:{} en el catálogo.".format(isbn)
         else:
-            biblioteca_libros.append(Libro(titulo,autor,isbn,True))
-            return "Libro agregado con éxito."
+            biblioteca_libros.append(Libro(titulo,autor,isbn))
+            return f"Libro agregado con éxito."
         
     def prestarLibro(self, isbn: str):
         libro = self.getLibro(isbn)
@@ -75,11 +75,11 @@ class Biblioteca:
         if libro is not None:
             if libro.disponible:
                 libro.prestar()
-                return "Libro prestado con éxito"
+                return f"Libro prestado con éxito."
             else:
-                return "Este libro se encuentra actualmente prestado"
+                return f"Este libro se encuentra actualmente prestado."
         else:
-            return "El libro de ISBN:{} no esiste".format(isbn)
+            return f"El libro de ISBN:{} no esiste.".format(isbn)
         
     def devolverLibro(self, isbn: str):
         libro = self.getLibro(isbn)
@@ -87,36 +87,42 @@ class Biblioteca:
         if libro is not None:
             if not libro.disponible:
                 libro.devolver()
-                return "Libro devuelto con éxito"
+                return f"Libro devuelto con éxito."
             else:
-                return "Este libro no estaba prestado anteriormente"
+                return f"Este libro no estaba prestado anteriormente."
         else:
-            return "El libro de ISBN:{} no esiste".format(isbn)
+            return f"El libro de ISBN:{} no esiste.".format(isbn)
         
     def mostrarLibros(self):
         lista_libros = ""
         for libro in biblioteca_libros:
-                lista_libros = str(libro) + "\n"
+                lista_libros += str(libro) + "\n"
         
         return lista_libros
 
 #Clase biblioteca --------------------------------------------
 # ------------------------------------------------------------
 
+def cobertura():
+    libro_palo = Libro('','','')
+    libro_palo.buscar('')
+    libro_palo.mostrar('')
+
 def gestion_inventario():
 
+    #Inicializamos la biblioteca
     biblioteca = Biblioteca()
 
     seguir = "SI"
 
-    texto_menu = "Bienvenido al Sistema de Gestión de Biblioteca\n\n"
-    linea_agregar = "1. Agregar libro\n"
-    linea_prestar = "2. Prestar libro\n"
-    linea_devolver = "3. Devolver libro\n"
-    linea_mostrar = "4. Mostrar libros\n"
-    linea_buscar = "5. Buscar\n"
-    linea_salir = "6. Salir\n\n"
-    linea_elegir_opcion = "Elige una opción: " 
+    texto_menu = f"Bienvenido al Sistema de Gestión de Biblioteca\n\n"
+    linea_agregar = f"1. Agregar libro\n"
+    linea_prestar = f"2. Prestar libro\n"
+    linea_devolver = f"3. Devolver libro\n"
+    linea_mostrar = f"4. Mostrar libros\n"
+    linea_buscar = f"5. Buscar\n"
+    linea_salir = f"6. Salir\n\n"
+    linea_elegir_opcion = f"Elige una opción: " 
 
     print (texto_menu + linea_agregar + linea_prestar + linea_devolver + linea_mostrar + linea_buscar + linea_salir)
 
@@ -124,7 +130,7 @@ def gestion_inventario():
         entrada_usuario = input(linea_elegir_opcion)
 
         if entrada_usuario == "6":
-            print(f"Salimos de la aplicación")
+            print(f"Salimos de la aplicación.")
             seguir = "NO"
         # AGREGAR UN LIBRO
         elif entrada_usuario == "1":
@@ -145,7 +151,11 @@ def gestion_inventario():
             print(biblioteca.prestarLibro(isbn))
         # MOSTRAR TODOS LOS LIBROS
         elif entrada_usuario == "4":
-            print(biblioteca.mostrarLibros())
+            listado_libros =  biblioteca.mostrarLibros()
+            if listado_libros == "":
+                print(f"Aun no hay libros en la biblioteca.")
+            else:
+                print(listado_libros)
         # BUSCAR UN LIBRO
         elif entrada_usuario == "5":
             isbn = input("Introduce el ISBN del libro: ")
@@ -154,10 +164,12 @@ def gestion_inventario():
             if libro_encontrado is not None:
                 print(str(libro_encontrado))
             else:
-                print ("Libro no encontrado en la biblioteca")
+                print (f"Libro no encontrado en la biblioteca.")
         # ENTRADAS NO VÁLIDAS
         else:
-            print("Los valores válidos son de 1,2,3,4,5 y 6 para Salir")
+            print(f"Opción introducida incorrecta; los valores válidos son 1,2,3,4,5 y 6 para Salir")
+
+#Acciones de palo para cubrir tod la cobertura de código
 
 
 gestion_inventario()
